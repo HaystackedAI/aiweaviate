3e6967b65fabe9ba79ee8cabbd9aedac904ae44c
aiaccfin <aiaccfin@gmail.com>
2026-05-05 17:14:13 -0400

    g

--- app/query/vectorsearch.py
+++
@@ -1,9 +0,0 @@
-def search(client, vector, limit=3):
-    collection = client.collections.get("Doc")
-
-    result = collection.query.near_vector(
-        near_vector=vector,
-        limit=limit
-    )
-
-    return [obj.properties for obj in result.objects]
--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-# main.py
-from app.core.wvclient import get_client
-from app.schema.wvschema import create_schema
-from app.data.ingestion import insert_doc
-from app.query.vectorsearch import search
+from fastapi import FastAPI
+app = FastAPI()
 
-client = get_client()
-
-try:
-    print("Connected:", client.is_ready())
-
-    create_schema(client)
-
-    insert_doc(
-        client,
-        "Sanofi builds biomedical AI systems",
-        "doc1",
-        [0.1, 0.2, 0.3]
-    )
-
-    results = search(client, [0.1, 0.2, 0.3])
-
-    print("\nRESULTS:")
-    for r in results:
-        print(r)
-
-finally:
-    client.close()
+@app.get("/")
+def main():
+    return {"message": "Hello World"}
