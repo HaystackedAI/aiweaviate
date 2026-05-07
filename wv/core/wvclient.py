56c3f85410929c87bfc111f717a09cb935506fef
aiaccfin <aiaccfin@gmail.com>
2026-05-07 14:47:57 -0400

    update

--- wv/core/wvclient.py
+++ wv/core/wvclient.py
@@ -4,8 +4,7 @@ def get_client():
     client = weaviate.connect_to_local(
         host="localhost",
         port=8080,
-        skip_init_checks=True
-        # grpc_port=50051
+        grpc_port=50051
     )
 
     return client
